0e5eae9250d63dfd9238983b2128d583bc40ba30
cuzz <775046211@qq.com>
2018-05-24 11:57:32 +0800

    Update: suback done

--- generate.js
+++ generate.js
@@ -54,6 +54,8 @@ module.exports = class Generate {
   connect () {
     // Todo: 客户端处实现
   }
+
+  /* 连接确认 */
   connack () {
     let returnCode = this._packet.returnCode
     let sessionPresent = this._packet.sessionPresent
@@ -69,6 +71,51 @@ module.exports = class Generate {
     pos += this.writeLength(buffer, pos, 2)
     buffer.writeInt8((sessionPresent && protocol.SESSIONPRESENT_MASK) || 0, pos++, true)
     buffer.writeInt8(returnCode, pos++, true)
+
+    return buffer
+  }
+
+  /* 订阅确认 */
+  suback () {
+    let messageId = this._packet.messageId
+    let granted = this._packet.granted
+
+    let length = 0
+    let pos = 0
+
+    if (typeof messageId !== 'number') {
+      utils.showError('Invalid message id ')
+    }
+
+    length += 2
+
+    // check QOS vector
+    if (Array.isArray(granted) && granted.length) {
+      for (let i = 0; i < granted.length; i++) {
+        if (typeof granted[i] !== 'number') {
+          utils.showError('Invalid QOS vector')
+        }
+        length += 1
+      }
+    } else {
+      utils.showError('Invalid QOS vector')
+    }
+
+    let buffer = Buffer.alloc(1 + this.calcLengthLength(length) + length)
+
+    // Header
+    buffer.writeInt8(protocol.codes['suback'] << protocol.CMD_SHIFT, pos++, true)
+
+    // Length 写入剩余长度
+    pos += this.writeLength(buffer, pos, length)
+
+    // Message Id
+    pos += this.writeNumber(buffer, pos, messageId)
+
+    // Subscriptions
+    granted.forEach(val => {
+      buffer.writeInt8(val, pos++, true)
+    })
 
     return buffer
   }
@@ -96,5 +143,42 @@ module.exports = class Generate {
     } while (length > 0)
 
     return pos - origPos
+  }
+
+  /**
+   * writeNumber - write a two byte number to the buffer
+   *
+   * @param <Buffer> buffer - destination
+   * @param <Number> pos - offset
+   * @param <String> number - number to write
+   * @return <Number> number of bytes written
+   *
+   * @api private
+   */
+  writeNumber (buffer, pos, number) {
+    buffer.writeUInt8(number >> 8, pos, true)
+    buffer.writeUInt8(number & 0x00FF, pos + 1, true)
+
+    return 2
+  }
+
+  /**
+   * calcLengthLength - calculate the length of the remaining
+   * length field
+   *
+   * @api private
+   */
+  calcLengthLength (length) {
+    if (length >= 0 && length < 128) {
+      return 1
+    } else if (length >= 128 && length < 16384) {
+      return 2
+    } else if (length >= 16384 && length < 2097152) {
+      return 3
+    } else if (length >= 2097152 && length < 268435456) {
+      return 4
+    } else {
+      return 0
+    }
   }
 }
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 let Generate = require('./generate')
 
-let packet = {
+// 模拟一个服务端发送的连接确认包
+// expect [32, 2, 0, 0]
+let connackPacket = {
   returnCode: 0,
   sessionPresent: false,
   cmd: 'connack'
 }
 
+// 模拟一个服务端发送的订阅确认包
+// expect [144, 3, 78, 182, 0]
+let subackPacket = {
+  messageId: 20150,
+  granted: [0],
+  cmd: 'suback'
+}
+
 let gen = new Generate()
-let buffer = gen.generate(packet)
-console.log(buffer)
+let connackBuffer = gen.generate(connackPacket)
+let subackBuffer = gen.generate(subackPacket)
+console.log(connackBuffer) // [32, 2, 0, 0]
+console.log(subackBuffer) // [144, 3, 78, 182, 0]
